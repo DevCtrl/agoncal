332b6988186f2afe83a642f2bfbe379f0bc88f82
Rob Kovtiuk <robsonheater@gmail.com>
2016-12-15 17:24:15 +0200

    Init commit

---
+++ src/main/java/com/devctrl/agoncal/data/entity/Book.java
@@ -0,0 +1,51 @@
+package com.devctrl.agoncal.data.entity;
+
+public class Book {
+
+    private String title;
+    private Float price;
+    private String discritpion;
+    private String number;
+
+    public Book() {
+    }
+
+    public Book(String title, Float price, String discritpion, String number) {
+        this.title = title;
+        this.price = price;
+        this.discritpion = discritpion;
+        this.number = number;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDiscritpion() {
+        return discritpion;
+    }
+
+    public void setDiscritpion(String discritpion) {
+        this.discritpion = discritpion;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
